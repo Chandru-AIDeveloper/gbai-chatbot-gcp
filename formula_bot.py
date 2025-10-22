c4b35a2be0fa7c3a4f4340ac3efa2d1295997405
Chandru Jaiganesh <jpjchandru@gmail.com>
2025-10-22 10:29:42 +0530

    FIX: Add correct base_url to all specialized bots

--- formula_bot.py
+++ formula_bot.py
@@ -4,7 +4,7 @@ import logging
 import traceback
 import re
 import pandas as pd
-# from langchain_ollama import ChatOllama
+from langchain_ollama import ChatOllama
 from typing import List
 from fastapi import FastAPI, Header
 from fastapi.responses import JSONResponse
@@ -48,11 +48,11 @@ app.add_middleware(
 )
 
 # Get the Ollama URL from an environment variable, defaulting to localhost for local development
-# llm = ChatOllama(
-#     model="gemma:2b",
-#     base_url="http://ollama:11434",
-#     temperature=0.3
-# )
+llm = ChatOllama(
+    model="gemma:2b",
+    base_url="http://ollama:11434",
+    temperature=0.3
+)
 
 def load_csv_as_document(file_path: str) -> Document:
     try:
@@ -171,7 +171,7 @@ if retriever:
             context=lambda x: format_docs(retriever.invoke(x["question"]))
         )
         | prompt
-        # | llm
+        | llm
         | StrOutputParser()
     )
 
